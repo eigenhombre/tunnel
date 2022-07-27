ce00a4a253e2f62f35feac638af098d2ccddfde1
John Jacobsen <eigenhombre@gmail.com>
2022-07-27 11:17:01 -0500

    Movement!  Messages!

--- oned/main.py
+++ oned/main.py
@@ -3,18 +3,25 @@ import random
 
 def print_help():
     print("""
-? - help
-t - teleport
-q - quit
+                       ? - help
+                       t - teleport
+                       . - wait
+                       q - quit
+left arrow / right arrow - move
 
 """)
 
 class Game:
     def __init__(self, term):
-        assert term.width > 10, "terminal too narrow"
-        self.tunnel_length = random.randint(10, term.width)
-        self.player_pos = random.randint(0, self.tunnel_length)
+        assert term.width > 15, "terminal too narrow"
+        self.tunnel_length = random.randint(10, min(max(15, term.width),
+                                                    70))
         self.term = term
+        self.message = "? for help"
+        self.__newpos()
+
+    def __newpos(self):
+        self.player_pos = random.randint(0, self.tunnel_length - 1)
 
     def draw_player(self):
         with (self.term.hidden_cursor(),
@@ -30,7 +37,7 @@ class Game:
         with self.term.location(0, self.term.height - 2):
             print('Welcome to '
                   + self.term.underline('the tunnel')
-                  + f' (? for help)!', end='')
+                  + f' ({self.message})!', end='')
         self.draw_tunnel()
         self.draw_player()
 
@@ -42,11 +49,29 @@ class Game:
         while True:
             with self.term.cbreak(), self.term.hidden_cursor():
                 inp = self.term.inkey()
-                if inp.lower() == "q":
+                linp = inp.lower()
+                if linp == "q":
                     break
-                elif inp.lower() == "?":
+                elif linp == "?":
                     print_help()
                     self.draw_game()
+                elif linp == "t":
+                    self.__newpos()
+                elif linp == ".":
+                    pass
+                elif inp.name == "KEY_LEFT":
+                    self.player_pos = max(0, self.player_pos - 1)
+                    self.draw_tunnel()
+                    self.draw_player()
+                elif inp.name == "KEY_RIGHT":
+                    self.player_pos = min(self.tunnel_length-1,
+                                          self.player_pos + 1)
+                    self.draw_tunnel()
+                    self.draw_player()
+                else:
+                    self.message = f"Key pressed was {inp.name}"
+                    self.draw_game()
+                self.draw_game()
         print()
         print("DONE")
 
